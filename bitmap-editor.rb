3f572c7560b2e9198f04464d6eed48ae874ecdb5
Frank Mckenna <fmckenna56330@gmail.com>
2022-05-16 13:24:05 +0100

    chore: prevent the user from creating an incompatible canvas size

--- bitmap-editor.rb
+++ bitmap-editor.rb
@@ -3,6 +3,8 @@ require_relative 'help'
 @bitmap
 @canvasX
 @canvasY
+MAX_PIXELS = 250
+MIN_PIXELS = 1
 
 def print_menu 
   puts "S: Show the contents of the current canvas"
@@ -21,8 +23,10 @@ def process(selection)
   case selection
   when "S" then print_canvas
   when "?" then print_help
-  when /^I\s\d+\s\d+$/ then create_canvas(selection)
-  when /^L\s\d+\s\d+\s[A-Z]{1}$/ then color_pixels(selection)
+  when /^I\s\d+\s\d+$/
+    create_canvas(selection)
+  when /^L\s\d+\s\d+\s[A-Z]{1}$/
+    color_pixels(selection)
   when "C" then clear_canvas
   when "X" then exit
   end
@@ -34,12 +38,24 @@ end
 
 def create_canvas(selection)
   coord = selection.scan(/[0-9]+/).map(&:to_i)
+  puts "#{coord}"
   @canvasX = coord[0]
   @canvasY = coord[1]
+
+  if @canvasX > MAX_PIXELS || @canvasY > MAX_PIXELS || 
+     @canvasX < MIN_PIXELS || @canvasY < MIN_PIXELS
+    puts "A canvas can only be between 1 and 250 pixels large"
+    return
+  end
+
   @bitmap = Array.new(@canvasX) { Array.new(@canvasY, 0) }
 end
 
 def color_pixels(selection)
+  if !@bitmap
+    puts "Please create a canvas first"
+    return
+  end
   color = selection[selection.length - 1]
   coord = selection.scan(/[0-9]+/).map(&:to_i)
   x = coord[0] - 1
